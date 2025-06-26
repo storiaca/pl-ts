bc49430ad0d7c6b499a36e414ce4b873e451e63b
Aleksandar Ristic <storiaca@gmail.com>
2025-06-26 21:04:32 +0200

    Tipovi u ts-u

--- ts-learn/src/index.ts
+++ ts-learn/src/index.ts
@@ -4,5 +4,12 @@ let age: number = 40;
 let professor: boolean = false;
 
 let grades: number[] = [3, 5, 7, 8, 10];
+let students: string[] = ["Aleksandar", "Petar", "Jovan", "Maja"];
+
+let username: string | boolean = "Toma";
+
+// Vezba: Napraviti niz koji moze biti skup brojeva ili stringova:
+
+let practiceArr: (number | string)[] = ["Pera", 5];
 
 console.log(name, age, professor);
